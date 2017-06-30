37ddd905d059838ed9d5b9479a7f3904d4342933
Bradley Stachurski <bradley.stachurski@gmail.com>
2017-06-29 22:33:17 -0500

    Added editStore and updateStore controllers

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
-const h = require('../helpers');
 
 exports.homePage = (req, res) => {
   res.render('index');
@@ -12,11 +11,25 @@ exports.addStore = (req, res) => {
 
 exports.createStore = async (req, res) => {
   const store = await (new Store(req.body)).save();
-  req.flash('info', `Successfully created ${store.name}. Care to leave a review?`);
+  req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
   res.redirect(`/store/${store.slug}`);
 };
 
 exports.getStores = async (req, res) => {
   const stores = await Store.find();
   res.render('stores', { title: 'Stores', stores })
+};
+
+exports.editStore = async (req, res) => {
+  const store = await Store.findOne({_id: req.params.id});
+  res.render('editStore', { title: `Edit ${store.name}`, store })
+};
+
+exports.updateStore = async (req, res) => {
+  const store = await Store.findOneAndUpdate({_id: req.params.id}, req.body, {
+    new: true, // return the new store instead of the old one
+    runValidators: true
+  }).exec();
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store</a>`);
+  res.redirect(`/stores/${store._id}/edit`)
 };
