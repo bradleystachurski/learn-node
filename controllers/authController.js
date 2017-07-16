f96d99d64ca5dc9215f6a99be3e1e7a7d9259d5d
Bradley Stachurski <bradley.stachurski@gmail.com>
2017-07-16 14:49:44 -0500

    Ability to generate reset password token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,7 @@
 const passport = require('passport');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
 
 exports.login = passport.authenticate('local', {
   failureRedirect: '/login',
@@ -20,5 +23,23 @@ exports.isLoggedIn = (req, res, next) => {
     return;
   }
   req.flash('error', 'Oops, you must be logged in to do that!');
+  res.redirect('/login');
+};
+
+exports.forgot = async (req, res) => {
+  // 1. Check if a user with that email exists
+  const user = await User.findOne({ email: req.body.email });
+  if(!user) {
+    req.flash('error', 'No account with that email exists');
+    return res.redirect('/login');
+  }
+  // 2. Set reset tokens and expiry on their account
+  user.resetPasswordToken = crypto.randomBytes(20).toString('hex');
+  user.resetPasswordExpires = Date.now() + 3600000; // 1 hour from now
+  await user.save();
+  // 3. Send them an email with the token
+  const resetURL = `https://${req.headers.host}/account/reset/${user.resetPasswordToken}`;
+  req.flash('success', `You have been emailed a password reset link. ${resetURL}`);
+  // 4. Redirect to login page
   res.redirect('/login');
 };
